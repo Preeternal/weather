5958be33e4ec941fea964c1be02c1f1e355d17e0
Preeternal <preeternal@gmail.com>
2022-08-21 10:19:44 +0300

    add some deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,36 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
+    root: true,
+    extends: ['@react-native-community', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier', 'import', 'module-resolver'],
+    settings: {
+        'import/resolver': {
+            'babel-module': {},
+        },
     },
-  ],
+    rules: {
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    'external',
+                    'internal',
+                    'index',
+                    'sibling',
+                    'parent',
+                    'builtin',
+                ],
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/no-shadow': ['error'],
+                'no-shadow': 'off',
+                'no-undef': 'off',
+            },
+        },
+    ],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['./src'],
+                extensions: [
+                    '.ios.ts',
+                    '.android.ts',
+                    '.ts',
+                    '.ios.tsx',
+                    '.android.tsx',
+                    '.tsx',
+                    '.jsx',
+                    '.js',
+                    '.json',
+                ],
+                alias: {
+                    '@screens': './src/screens',
+                    '@components': './src/components',
+                    '@theme': './src/theme',
+                    '@images': './src/assets/images',
+                    '@services': './src/services',
+                    '@navigation': './src/navigation',
+                    '@config': './src/config',
+                    '@state': './src/state',
+                    '@hooks': './src/hooks',
+                },
+            },
+        ],
+    ],
 };
